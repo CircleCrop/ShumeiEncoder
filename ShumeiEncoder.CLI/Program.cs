f8658f503f844304f2ede6272dd9f2d72dc13043
CropCircle <circlecrop@outlook.com>
2025-01-15 09:55:43 +0800

    调整 CLIApi 输入处理

--- ShumeiEncoder.CLI/Program.cs
+++ ShumeiEncoder.CLI/Program.cs
@@ -4,16 +4,21 @@ using YamlDotNet.Serialization;
 public class Program {
     internal static void Main(string[] args) {
 
-        Console.WriteLine("Welcome to ShumeiEncoder");
-        Console.WriteLine();
+        Console.WriteLine("Welcome to ShumeiEncoder\n");
 
         /*Console.WriteLine("Please input a file...");
         string? newInputPath = Console.ReadLine();
         if (newInputPath == null) {
             return;
-        }
-        */
+        }*/
 
+        /*
+         * 主菜单 - ：输入文件
+         * 子菜单 - 文件处理：1. 添加音/视频流；2. 选择预设。选择 1 后循环一次菜单，选择 2 进入预设载入。
+         * 子菜单 - 预设选择：交互对话框，读取到变量。
+         * 子菜单 - 执行确认
+         * 子菜单 - 按任意键退出
+         */
         // 选择文件和预设
         string filePath = CLIApi.ChooseFile("Input File: ");
 
@@ -28,18 +33,29 @@ public class Program {
         Preset preset = deserializer.Deserialize<Preset>(yaml);
         Console.WriteLine($"{preset.Name}: {preset.Description ?? ""}");
 
-        StringBuilder VideoEncodeArgs;
         string cachePath;
+        // 视频部分
         string videoCodec;
-        BuildVideoEncodeArgs(outputPath, preset, out videoCodec, out VideoEncodeArgs, out cachePath);
+        StringBuilder VideoEncodeArgs;
+        BuildVideoEncodeArgs(outputPath, preset, out videoCodec, out cachePath, out VideoEncodeArgs);
 
+        // 音频部分
+        string audioCodec;
+        StringBuilder AudioEncodeArgs;
+        BuildAudioEncodeArgs(filePath, preset, cachePath, AudioEncodeArgs);
+    }
+
+    private static void BuildAudioEncodeArgs(string filePath,
+                                             Preset preset,
+                                             string cachePath,
+                                             out StringBuilder AudioEncodeArgs,
+                                             out string cacheFilePath) {
         if (preset.Audio?.Fmt != null && preset.Audio.Fmt == "aac") {
             // 读取音频位深，避免 16 位量化误差【To Do】
             StringBuilder AudioEncodeCommand =
                 new($"ffmpeg -i {filePath} -f wav - | qaac64 --tvbr {preset.Audio.Args?["quality"] ?? "127"} -o {cachePath}\\output_cache.m4a\" -");
             // 支持其他 AAC 参数【To Do】
-            Console.WriteLine(VideoEncodeArgs.ToString());
-            Console.WriteLine();
+
         }
 
         if (preset.Audio?.Fmt != null && preset.Audio.Fmt == "flac") {
@@ -54,12 +70,12 @@ public class Program {
     private static void BuildVideoEncodeArgs(string outputPath,
                                              Preset preset,
                                              out string codec,
-                                             out StringBuilder VideoEncodeArgs,
-                                             out string cachePath) {
-        //ffmpeg -i input.avs -f yuv4mpegpipe -an -v 0 - | x264 [options] --demuxer y4m -o output.264 -
-        codec = "";
+                                             out string cacheStreamFilePath,
+                                             out StringBuilder VideoEncodeArgs) {
+        //ffmpeg -i input.mp4 -f yuv4mpegpipe -an -v 0 - | x264 [options] --demuxer y4m -o output.264 -
         VideoEncodeArgs = new();
-        cachePath = outputPath[..outputPath.LastIndexOf('\\')];
+        cacheStreamFilePath = Path.Combine(Path.GetDirectoryName(outputPath)
+                                           ?? "", "output_cache.264") ?? "";
 
         // 构造编码参数
         if (preset.Video?.Fmt != null && preset.Video?.Args != null) {
@@ -67,13 +83,15 @@ public class Program {
             ArgsToCLIString(preset, VideoEncodeArgs);
             if (preset.Video.Fmt == "h264" || preset.Video.Fmt == "avc") {
                 codec = "x264";
-                VideoEncodeArgs.Append($" -o {cachePath}\\output_cache.264\" -");
+                VideoEncodeArgs.Append($" -o \"{cacheStreamFilePath}\" -");
             } else if (preset.Video.Fmt == "h265" || preset.Video.Fmt == "hevc") {
                 codec = "x265";
                 //
+            } else {
+                codec = "";
             }
 
-            Console.WriteLine($"VideoEncodeArgs: {VideoEncodeArgs.ToString()}\n");
+            //Console.WriteLine($"VideoEncodeArgs: {VideoEncodeArgs.ToString()}\n");
         }
     }
     private static void ArgsToCLIString(Preset preset, StringBuilder VideoEncodeCommand) {
@@ -84,29 +102,21 @@ public class Program {
 }
 
 
-
 internal class CLIApi() {
-    /*
-     * 主菜单 - ：输入文件
-     * 子菜单 - 文件处理：1. 添加音/视频流；2. 选择预设。选择 1 后循环一次菜单，选择 2 进入预设载入。
-     * 子菜单 - 预设选择：交互对话框，读取到变量。
-     * 子菜单 - 执行确认
-     * 子菜单 - 按任意键退出
-     */
     public static string ChooseFile(string prompt) {
         Console.Write(prompt);
-        string? path = Console.ReadLine();
+        string path = Console.ReadLine() ?? "";
         Console.WriteLine();
-        if (!string.IsNullOrWhiteSpace(path)) {
-            return path.Trim() ?? "";
+        if (string.IsNullOrWhiteSpace(path)) {
+            return path.Trim().Trim('\"', '\'');
         } else {
             return "";
         }
     }
     public static Boolean CheckStart() {
         Console.Write("Start Processing? (y/n) ");
-        string? input = Console.ReadLine();
-        return input?.Trim() switch {
+        string input = Console.ReadLine() ?? "";
+        return input.Trim() switch {
             "y" => true,
             _ => false,
         };
